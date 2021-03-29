b3cdbaa78fa07b2b648c0208ca869ac33565b71b
Tomas <eliz.tomas23@gmail.com>
2021-03-29 15:30:43 +0200

    unused code deleted

--- middleware/sanitizing.js
+++ middleware/sanitizing.js
@@ -1,23 +1,3 @@
-exports.capitalizeUser = (req, res, next) => {
-  const { firstName, lastName } = req.body;
-  req.body.firstName = firstName[0].toUpperCase().concat(firstName.slice(1));
-  req.body.lastName = lastName[0].toUpperCase().concat(lastName.slice(1));
-  next();
-};
-
-exports.sortBands = (req, res, next) => {
-  if (req.body.favoriteBands.length) {
-    req.body.favoriteBands.sort();
-    next();
-  }
-};
-exports.strToNum = (req, res, next) => {
-  const { age, fbw } = req.body;
-  req.body.age = parseInt(age);
-  req.body.fbw = parseInt(fbw);
-  next();
-};
-
 const { body } = require("express-validator");
 
 exports.sanitizeUser = () => {
